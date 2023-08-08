2b12f667850f6322ca3331c371a3b809a8232fbe
Daniel.Barda <danielbarda97@gmail.com>
2023-08-08 12:55:45 +0300

    test update_version

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+show_help() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  -e, --echo      Echo the new version"
+    echo "  -r, --replace   Run sed to replace version in pom.xml"
+    echo "  -h, --help      Show this help message"
+}
+
 current_version=$(awk -F'[<>]' '/<artifactId>my-app<\/artifactId>/{getline; getline; print $3}' pom.xml)
 
 IFS='.-' read -ra version_parts <<< "$current_version"
@@ -11,6 +19,25 @@ patch_version=$((patch_version + 1))
 
 new_version="$major_version.$minor_version.$patch_version-SNAPSHOT"
 
-echo $new_version
-
-# sed -i "s/<version>$current_version<\/version>/<version>$new_version<\/version>/" pom.xml
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -e|--echo)
+            echo "$new_version"
+            shift
+            ;;
+        -r|--replace)
+            sed -i "s/<version>$current_version<\/version>/<version>$new_version<\/version>/" pom.xml
+            echo "Version replaced in pom.xml"
+            shift
+            ;;
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        *)
+            echo "Invalid option: $1"
+            show_help
+            exit 1
+            ;;
+    esac
+done
